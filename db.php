2f665218365a21ba971159280ad09213f88ece62
Penguin <son@vdburg.site>
2025-01-15 09:57:41 +0100

    db progress

---
+++ db.php
@@ -0,0 +1,27 @@
+<?php
+
+// --- This file is used to connect all webpages to the database.
+// --- Change these values if you need to log into a different acount then 'bit_academy'.
+
+// set access data
+$dbhost = 'localhost';
+$dbname = 'gameforge';
+$dbuser = 'bit_academy';
+$dbpass = 'bit_academy';
+$charset = 'utf8mb4';
+
+// DSN (Data Source Name)
+$dsn = "mysql:host=$dbhost;dbname=$dbname;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => true,
+];
+
+// create a PDO instance
+try {
+    $pdo = new PDO($dsn, $dbuser, $dbpass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
