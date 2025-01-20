cbcd264eb4f9f159ab11ba01aedb96d8515889d1
Snevver <svenhoeksema@hotmail.com>
2025-01-20 12:02:14 +0100

    Update import.sql

--- import.sql
+++ import.sql
@@ -1,73 +1,26 @@
-DROP DATABASE IF EXISTS Nexed2;
-CREATE DATABASE Nexed2;
-USE Nexed2;
+DROP DATABASE IF EXISTS Rhizome;
+CREATE DATABASE Rhizome;
+USE Rhizome;
 
-CREATE TABLE `Students` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `age` int(11) NOT NULL,
-  `birthdate` date NOT NULL,
-  `total_xp` int(11) NOT NULL,
-  `level` int(11) NOT NULL,
-  `absences` int(11) NOT NULL,
-  `completed_modules` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Coaches` (
+CREATE TABLE `Accounts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
+  `isAdmin` BOOLEAN NOT NULL,
+  `class` INT(11) NULL,
+  `total_xp` int(11) NULL,
+  `completedModules` varchar(255) NULL,
+  `sickDays` int(11) NULL,
+  `absentDays` int(11) NULL,
+  `lateDays` int(11) NULL,
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Modules` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `level_amount` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `Classes` (
+  `classId` int(11) NOT NULL AUTO_INCREMENT,
+  `homework` varchar(255) NOT NULL,
+  `examName` varchar(255) NOT NULL,
+  `examDate` date NOT NULL,
+  PRIMARY KEY (`classId`)
 );
-
-INSERT INTO `Modules` (`name`, `description`, `level_amount`) VALUES
-  ('PHP Beginner (Placeholder)', 'Begin met het leren van PHP!', 3),
-  ('PHP Advanced (Placeholder)', 'Ga verder met het leren van PHP!', 5),
-  ('JavaScript Beginner (Placeholder)', 'Begin met het leren van JavaScript!', 4),
-  ('JavaScript Advanced (Placeholder)', 'Ga verder met het leren van JavaScript!', 3);
-  ('JavaScript Beginner (Placeholder)', 'Begin met het leren van JavaScript!', 4),
-  ('JavaScript Advanced (Placeholder)', 'Ga verder met het leren van JavaScript!', 3);
-
-CREATE TABLE `Levels` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `xp_amount` int(11) NOT NULL,
-  `level_code` varchar(255) NOT NULL,
-  `module_id` int(11) NOT NULL,
-  FOREIGN KEY (`module_id`) REFERENCES `Modules`(`id`),
-  PRIMARY KEY (`id`)
-);
-
-INSERT INTO `Levels` (`name`, `description`, `xp_amount`, `levelIndex`, `module_id`) VALUES
-  ('PHP Beginner (Placeholder)', 'Het eerste level', 10, 1, 1),
-  ('PHP Beginner (Placeholder)', 'Het tweede level', 20, 2, 1),
-  ('PHP Beginner (Placeholder)', 'Het derde level', 30, 3, 1),
-  ('PHP Advanced (Placeholder)', 'Het eerste level', 10, 1, 2),
-  ('PHP Advanced (Placeholder)', 'Het tweede level', 20, 2, 2),
-  ('PHP Advanced (Placeholder)', 'Het derde level', 30, 3, 2),
-  ('PHP Advanced (Placeholder)', 'Het vierde level', 40, 4, 2),
-  ('PHP Advanced (Placeholder)', 'Het vijfde level', 50, 5, 2),
-  ('JavaScript Beginner (Placeholder)', 'Het eerste level',  10, 1, 3),
-  ('JavaScript Beginner (Placeholder)', 'Het tweede level', 20, 2, 3),
-  ('JavaScript Beginner (Placeholder)', 'Het derde level', 30, 3, 3),
-  ('JavaScript Beginner (Placeholder)', 'Het vierde level', 40, 4, 3),
-  ('JavaScript Advanced (Placeholder)', 'Het eerste level', 10, 5, 4),
-  ('JavaScript Advanced (Placeholder)', 'Het tweede level', 20, 6, 4),
-  ('JavaScript Advanced (Placeholder)', 'Het derde level', 30, 7, 4);
